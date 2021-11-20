2b718739ac9a2e1880aad9b25e08b5616d12ba17
Piotr Karolak <karolak.piotr.012@gmail.com>
2021-11-20 08:53:52 +0100

    di-setters

--- src/main/java/com/skni/warsztatysghv2/registration/ApplicationService.java
+++ src/main/java/com/skni/warsztatysghv2/registration/ApplicationService.java
@@ -1,6 +1,12 @@
 package com.skni.warsztatysghv2.registration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ApplicationService {
+    @Autowired
+    ApplicationService(){}
     public Application createMock() {
         return new Application("Mikolaj", "Zimkowski", "email@gmail.com"); // new allowed here
     }
